ea8dc8fc7a6e640c50a8f7be57e5cca8b0ae030b
Nick <nick@Nicholas-MacBook-Pro.local>
2021-03-16 11:17:25 +0000

    added config.py

---
+++ capstone/config.py
@@ -0,0 +1,10 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Enable debug mode.
+DEBUG = True
+# Database connection string
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:@localhost:5432/capstone'
+# Supress warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- capstone/models.py
+++ capstone/models.py
@@ -11,8 +11,9 @@ db = SQLAlchemy()
 # setup_db(app)
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object('config')
     db.app=app
     db.init_app(app)
     migrate = Migrate(app, db)
